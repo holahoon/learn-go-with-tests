93b57ef7bd9612f5d8e781c43dd1745620bc332c
DK <hola.hoon@gmail.com>
2024-08-24 08:50:58 -0400

    refactor test code

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("DK")
-	want := "Hello, DK"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("DK")
+		want := "Hello, DK"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello World' when an empty strin gis supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("Got %q, Want %q", got, want)
 	}
 }
