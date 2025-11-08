4162f47eb50ac15efc8789fe01923c0b96a8ce89
DK <hola.hoon@gmail.com>
2025-11-08 15:16:57 -0500

    feat: hello world

--- 1_hello_world/hello.go
+++ 1_hello_world/hello.go
@@ -1,11 +1,34 @@
 package main
 
-import "fmt"
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
-	return "hello " + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
 }
 
-func main() {
-	fmt.Println(Hello("David"))
+// A function with named return value (prefix string)
+// This will created a variable called "prefix" of type string in the function's scope
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	// instead of returning "prefix", we can just return and this will return the named return value
+	return
 }
+
+// func main() {
+// 	fmt.Println(Hello("David"))
+// }
--- 1_hello_world/hello_test.go
+++ 1_hello_world/hello_test.go
@@ -3,9 +3,39 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("David")
-	want := "hello David"
+	t.Run("sying hello to people", func(t *testing.T) {
+		got := Hello("David", "")
+		want := "Hello, David"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("DK", "French")
+		want := "Bonjour, DK"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// Tells the test suite that this function is a test helper.
+	// When it fails, the line number reported will be from the caller of this function.
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
